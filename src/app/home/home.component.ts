89dcee82d75c88dce2169cc32c886021018245db
Donchi Donald <donchidonald2000@gmail.com>
2023-10-06 18:47:00 +0200

    ngFor done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,15 +10,107 @@ import { HousingLocation } from '../housing-location';
 export class HomeComponent {
   readonly baseUrl = 'https://afrogeekacademy.com/assets/learn/img/';
 
-  housingLocation: HousingLocation = {
-    id: 9999,
-    name: 'Test Home',
-    city: 'Test city',
-    state: 'ST',
-    photo: `${this.baseUrl}/1.jpg`,
-    availableUnits: 99,
-    wifi: true,
-    laundry: false,
-  };
+  housingLocationList: HousingLocation[] = [
+    {
+        id: 0,
+        name: "Pixel Palace",
+        city: "Yaoundé",
+        state: "Centre",
+        photo: `${this.baseUrl}/1.jpg`,
+        availableUnits: 4,
+        wifi: true,
+        laundry: true
+    },
+    {
+        id: 1,
+        name: "Coder's Cove",
+        city: "Douala",
+        state: "Littoral",
+        photo: `${this.baseUrl}/2.jpg`,
+        availableUnits: 0,
+        wifi: false,
+        laundry: true
+    },
+    {
+        id: 2,
+        name: "Byte Bungalow",
+        city: "Bamenda",
+        state: "Northwest",
+        photo: `${this.baseUrl}/3.jpg`,
+        availableUnits: 1,
+        wifi: false,
+        laundry: false
+    },
+    {
+        id: 3,
+        name: "Algorithm Apartments",
+        city: "Buea",
+        state: "Southwest",
+        photo: `${this.baseUrl}/4.jpg`,
+        availableUnits: 1,
+        wifi: true,
+        laundry: false
+    },
+    {
+        id: 4,
+        name: "Bitstream Bunkers",
+        city: "Kribi",
+        state: "South",
+        photo: `${this.baseUrl}/5.jpg`,
+        availableUnits: 1,
+        wifi: true,
+        laundry: false
+    },
+    {
+        id: 5,
+        name: "Debug Dwellings",
+        city: "Ngaoundéré",
+        state: "Adamawa",
+        photo: `${this.baseUrl}/6.jpg`,
+        availableUnits: 2,
+        wifi: true,
+        laundry: true
+    },
+    {
+        id: 6,
+        name: "Script Suites",
+        city: "Garoua",
+        state: "Far North",
+        photo: `${this.baseUrl}/8.jpg`,
+        availableUnits: 5,
+        wifi: true,
+        laundry: true
+    },
+    {
+        id: 7,
+        name: "Syntax Sanctuary",
+        city: "Limbe",
+        state: "Southwest",
+        photo: `${this.baseUrl}/9.jpg`,
+        availableUnits: 2,
+        wifi: true,
+        laundry: true
+    },
+    {
+        id: 8,
+        name: "Hackers Haven",
+        city: "Foumban",
+        state: "West",
+        photo: `${this.baseUrl}/10.jpg`,
+        availableUnits: 10,
+        wifi: false,
+        laundry: false
+    },
+    {
+        id: 9,
+        name: "Logic Lofts",
+        city: "Ebolowa",
+        state: "South",
+        photo: `${this.baseUrl}/7.jpg`,
+        availableUnits: 6,
+        wifi: true,
+        laundry: true
+    }
+];
 
 }
