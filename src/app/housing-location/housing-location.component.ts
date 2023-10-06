89eb908574ccb794ee1a8c3c202d518c750fd1f5
Donchi Donald <donchidonald2000@gmail.com>
2023-10-06 18:14:52 +0200

    input output done

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-housing-location',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./housing-location.component.css']
 })
 export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
+  @Output() onDelete = new EventEmitter();
+
+  delete(i: number) {
+    this.onDelete.emit(i);
+  }
 
 }
