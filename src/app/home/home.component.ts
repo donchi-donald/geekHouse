168d33de652bfec7ffd148be32b0fb8bfd7ca696
Donchi Donald <donchidonald2000@gmail.com>
2023-10-06 17:59:16 +0200

    interface is done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HousingLocation } from '../housing-location';
+
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  readonly baseUrl = 'https://afrogeekacademy.com/assets/learn/img/';
+
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/1.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 
 }
